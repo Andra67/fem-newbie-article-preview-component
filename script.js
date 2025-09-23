041766e90eb1841da36a16b076c03d724e2daab5
Julien Andra <julien.andra01@gmail.com>
2025-09-23 23:01:07 +0700

    adding JS

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 shareButton = document.querySelector(".share-icon");
+
+shareContainer = document.querySelector(".share-container");
+profile = document.querySelector(".profile");
+
 shareButton.addEventListener("click", () => {
   document.querySelector(".social-media").classList.toggle("visible");
 });
